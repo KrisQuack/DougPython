c098fd8258772727247b5117628baea22e12fb30
krisquack <krisquack@outlook.com>
2024-01-09 16:28:17 +0000

    Store verification in DB

--- Cogs/Systems/Verification.py
+++ Cogs/Systems/Verification.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import random
 from datetime import datetime, timedelta, timezone
@@ -7,6 +8,7 @@ from discord import app_commands
 from discord.ext import commands, tasks
 
 from Classes.DiscordFunctions.ModActions import Timeout_User
+from Classes.Database.Members import get_member, update_member
 
 
 class Verification(commands.Cog):
@@ -45,35 +47,38 @@ Once you're all set, the realm of The Doug District is yours to explore.""",
 
     @tasks.loop(minutes=5)
     async def check_verification(self):
-        # Assign variables
-        ten_minutes_ago = (datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(minutes=60))
-        one_week_ago = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(weeks=1)
-        # Loop through all members in the server who do not have the member role
-        for member in self.client.guilds[0].members:
-            # If the member does not have the member role
-            if self.member_role in member.roles:
-                continue
-            # If the member is not a bot and does not have the new role
-            if not member.bot and self.new_role not in member.roles:
-                # If the member has been in the server for more than 10 minutes
-                if member.joined_at < ten_minutes_ago:
-                    # Kick the member for not being verified
-                    await member.kick(reason="Not verified")
-                else:
-                    # Send the member a reminder to verify
-                    await self.onboarding.send(
-                        f"{member.mention} you have not yet verified and will be kicked in the next 5 minutes if not complete",
-                        delete_after=60)
-            # If the member has been in the server for more than one week and has the new role
-            elif member.joined_at < one_week_ago and self.new_role in member.roles:
-                # Assign the member role to the member
-                await member.add_roles(self.member_role, reason="Graduated")
-        # Check if there are any messages in the onboarding channel older than 10 minutes
-        messages = [msg async for msg in self.onboarding.history(limit=100, after=ten_minutes_ago)]
-        for message in messages:
-            # If it is not the verification message
-            if message.id != 1158902786524721212:
-                await message.delete()
+        try:
+            # Assign variables
+            ten_minutes_ago = (datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(minutes=60))
+            one_week_ago = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(weeks=1)
+            # Loop through all members in the server who do not have the member role
+            for member in self.client.guilds[0].members:
+                # If the member does not have the member role
+                if self.member_role in member.roles:
+                    continue
+                # If the member is not a bot and does not have the new role
+                if not member.bot and self.new_role not in member.roles:
+                    # If the member has been in the server for more than 10 minutes
+                    if member.joined_at < ten_minutes_ago:
+                        # Kick the member for not being verified
+                        await member.kick(reason="Not verified")
+                    else:
+                        # Send the member a reminder to verify
+                        await self.onboarding.send(
+                            f"{member.mention} you have not yet verified and will be kicked in the next 5 minutes if not complete",
+                            delete_after=60)
+                # If the member has been in the server for more than one week and has the new role
+                elif member.joined_at < one_week_ago and self.new_role in member.roles:
+                    # Assign the member role to the member
+                    await member.add_roles(self.member_role, reason="Graduated")
+            # Check if there are any messages in the onboarding channel older than 10 minutes
+            messages = [msg async for msg in self.onboarding.history(limit=100, before=ten_minutes_ago)]
+            for message in messages:
+                # If it is not the verification message
+                if message.id != 1158902786524721212:
+                    await message.delete()
+        except Exception as e:
+            logging.getLogger("Verification").error(f"Failed to check verification: {e}")
 
     @check_verification.before_loop
     async def before_check_verification(self):
@@ -92,11 +97,15 @@ class VerifyButton(discord.ui.View):
         selected_file = random.choice(jpeg_files)
         # Extract the number (which serves as the answer) from the selected file's name
         answer = int(os.path.splitext(selected_file)[0].split('_')[-1])
+        # Store the answer in the database
+        dbUser = await get_member(interaction.user, interaction.client.database)
+        dbUser['verification'] = answer
+        await update_member(dbUser, interaction.client.database)
         # Create a Discord File object using the path to the selected JPEG file
         image = discord.File(os.path.join('Data/Verification', selected_file), f'{interaction.user.id}.jpg')
         # Send a message along with the randomly selected image and associated view for verification
         await interaction.response.send_message("## How many bell peppers do you see in the image above?",
-                                                view=VerifyAnswerView(answer), ephemeral=True, file=image)
+                                                view=VerifyAnswerView(), ephemeral=True, file=image)
 
 
 class VerifyAnswerButton(discord.ui.Button):
@@ -110,11 +119,12 @@ class VerifyAnswerButton(discord.ui.Button):
 
 
 class VerifyAnswerView(discord.ui.View):
-    def __init__(self, answer):
+    def __init__(self):
         super().__init__(timeout=None)
-        self.answer = answer
-        numbers = list(range(1, 11))  # Create a list of numbers from 1 to 10
-        random.shuffle(numbers)  # Shuffle the numbers
+        # Create a list of numbers from 1 to 10
+        numbers = list(range(1, 11))
+        # Shuffle the list
+        random.shuffle(numbers)
         # Define some colors you'd like to use
         colors = [
             discord.ButtonStyle.blurple,
@@ -130,7 +140,12 @@ class VerifyAnswerView(discord.ui.View):
             self.add_item(button)
 
     async def respond(self, interaction: discord.Interaction, option: int):
-        if option == self.answer:
+        # Get the answer from the database
+        dbUser = await get_member(interaction.user, interaction.client.database)
+        answer = dbUser['verification']
+        if option == answer:
+            dbUser.pop('verification')
+            await update_member(dbUser, interaction.client.database)
             role = interaction.guild.get_role(935020318408462398)
             await interaction.user.add_roles(role, reason="Verified")
             # If the user account is created less than one week, time out for the remainder of the week
